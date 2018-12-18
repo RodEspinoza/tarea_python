d0077e6c10c3cb49e38b5ac26ccd41c61e4b4965
novacane <espinoza.sample@gmail.com>
2018-12-17 23:38:10 -0300

    create tables and project objects in database

--- database.py
+++ database.py
@@ -1,13 +1,28 @@
 import sqlite3
 
+
 def generate_tables():
-    conn = sqlite3.connect(":memory:")
+    conn = sqlite3.connect("test.db")
     cur = conn.cursor()
     cur.execute(
-        "CREATE TABLE project (projectid INTEGER PRIMARY KEY)")
+        "CREATE TABLE IF NOT EXISTS project (projectid INTEGER PRIMARY KEY)")
     cur.execute(
-        "CREATE TABLE articles ("+
-        "token STRING PRIMARY KEY, project INTEGER, year INTEGER, country_name STRING)" +
-        "FOREING KEY (project) REFERENCES project(projectid)" )
+        "CREATE TABLE IF NOT EXISTS articles ("+
+        "token STRING PRIMARY KEY, project INTEGER, year INTEGER, country_name STRING," +
+        "FOREIGN KEY (project) REFERENCES project(projectid))" )
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS visits ("+
+        "page_path STRING, query_string STRING, visit_country STRING)"
+    )
     conn.commit()
     cur.close()
+    conn.close()
+
+def dummy_projects():
+    conn = sqlite3.connect("test.db")
+    cur = conn.cursor()
+    for i in range(20):
+        cur.execute("insert into project (projectid) values({})".format(i))
+    conn.commit()
+    cur.close()
+    conn.close()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from database import generate_tables
+from database import generate_tables, dummy_projects
 def main():
     generate_tables()
+    dummy_projects()
     print("nani")
 if __name__ == '__main__':
     main()
