c1fc93b0ba5b486b5c11334ecae54a1b3e9002db
novacane <espinoza.sample@gmail.com>
2018-12-18 16:23:07 -0300

    add forgotten sentence commit()

--- database.py
+++ database.py
@@ -41,16 +41,19 @@ def dummy_projects(MAX_PROJECTS):
 def insert_articles(project, token, year, country_name):
     conn = get_connection()
     cur = conn.cursor()
-    query = "insert into ARTICLES (project, token, year, country_name) values({},{},{},'{}')".format(project, token, year, country_name)
+    query = "insert into ARTICLES (project, token, year, country_name) values({},{},{},'{}')".format(
+        project, token, year, country_name)
     cur.execute(query)
+    conn.commit()
     cur.close()
     conn.close()
 
 def insert_visit(page_path, query_string, visit_country):
     conn = sqlite3.connect(database_name)
     cur = conn.cursor()
-    query = "insert into VISITS values(page_path, query_string, visit_country) values({}, {}, {})"
-        .values(page_path, query_string, visit_country)
+    query = "insert into VISITS (page_path, query_string, visit_country) values('{}', '{}', '{}')".format(page_path, query_string, visit_country)
+    cur.execute(query)
+    conn.commit()
     cur.close()
     conn.close()
 
